7a35112e3976ecdb45a4e8a3dc3778ef20ed43aa
razar <razear007@gmail.com>
2021-05-28 10:33:31 -0400

    fixes deployment issues

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker build -t razear/mega-electric-nginx:latest -t razear/mega-electric-nginx:
 docker push razear/mega-electric-nginx:latest
 docker push razear/mega-electric-nginx:$SHA
 
-fandogh service deploy --image razear/mega-electric-nginx --version latest --name mega-electric-nginx -p 80 -d
+fandogh service deploy --image razear/mega-electric-nginx --version $SHA --name mega-electric-nginx -p 80 -d
