3331c8b6a39aaecafc5e26da2bfda8c57df5f1c7
razar <razear007@gmail.com>
2021-06-09 09:41:35 -0400

    adding deployment yml file for fandogh cloud

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ docker build -t razear/mega-electric-nginx:latest -t razear/mega-electric-nginx:
 docker push razear/mega-electric-nginx:latest
 docker push razear/mega-electric-nginx:$SHA
 
-fandogh service deploy --image razear/mega-electric-nginx --version $SHA --name mega-electric-nginx -p 80 -d
+#fandogh service deploy --image razear/mega-electric-nginx --version $SHA --name mega-electric-nginx -p 80 -d
+fandogh service apply -f nginx-deployment.yml  \
+                 -p SHA=$SHA
